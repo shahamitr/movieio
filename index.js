3392b1c8b9b56d849ff9b9d24fac573d102ff5b4
Amit Shah <amit.r.shah.2005@gmail.com>
2017-02-25 01:23:25 +0530

    Added initial file required for bot

---
+++ index.js
@@ -0,0 +1,53 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: "c230e4e1-42b9-4a35-b30e-e024908950a5",
+    appPassword: "n7YkXwbnn5hp59WKESPe0em"
+});
+
+var bot = new builder.UniversalBot(connector);
+
+server.post('/api/messages', connector.listen());
+
+var optionData = {
+    "Movies": {
+        id: 1,
+        category: "Movies"
+    },
+    "TV Shows": {
+        id: 2,
+        category: "TV Shows"
+    },
+    "Celebrity": {
+        id: 3,
+        category: "Celebrity"
+    }
+};
+
+bot.dialog('/', [
+	function(session)  
+	{  
+		session.send("Hey there, I can help you with finding lots of things about following!!");
+		builder.Prompts.choice(session, "What would you like to search for?", optionData);
+	},
+	function (session, results) {
+        if (results.response) {
+            var selectedOption = optionData[results.response.entity];
+            session.send("Which %(category)s, you would like to know about.", selectedOption);
+        } else {
+            session.send("ok");
+        }
+    }
+]);
