7cd66c970d6278f3b29097498327a8d60f265973
Amit Shah <amit.r.shah.2005@gmail.com>
2017-02-26 10:57:15 +0530

    added os module

--- index.js
+++ index.js
@@ -15,10 +15,18 @@ var server = restify.createServer();
 console.log(server);
 
 var os = require("os");
-var hostname = os.hostname();
-
-console.log(os);
-
+console.log(os));
+console.log(os.hostname())
+console.log(os.type())
+console.log(os.platform())
+console.log(os.arch())
+console.log(os.release())
+console.log(os.uptime())
+console.log(os.loadavg())
+console.log(os.totalmem())
+console.log(os.freemem())
+console.log(os.cpus())
+console.log(os.networkInterfaces())
 
 server.listen(3900, function () {
    console.log('%s listening to %s', server.name, server.url); 
